fea7680a3d05d0553926c3bdc5d3639bb8af4ed4
RicardoBarretoR <richardbarrodriguez@hotmail.com>
2020-09-14 23:11:21 -0500

    adding task 0:returns information about list

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+"""returns information about his/her TODO list progress"""
+import json
+import requests
+import sys
+
+if __name__ == '__main__':
+    url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(url + 'users/{}'.format(sys.argv[1])).json()
+    todos = requests.get(url + 'todos', params={"userId": sys.argv[1]}).json()
+
+    completed = [O.get("title") for O in todos if O.get("completed") is True]
+    print("Employee {} is done with tasks({}/{}):".format(
+        user.get("name"), len(completed), len(todos)))
+
+    for task in completed:
+        print("\t {}".format(task))
