707a59967e5a68f2e3906b76b24197785077e7e5
RicardoBarretoR <richardbarrodriguez@hotmail.com>
2020-09-15 00:06:20 -0500

    task 3

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+"""export data in the JSON format"""
+import json
+import requests
+
+if __name__ == '__main__':
+    url = "https://jsonplaceholder.typicode.com/"
+    users = requests.get(url + 'users').json()
+
+with open("todo_all_employees.json", 'w') as f:
+        json.dump({user.get("id"): [{
+            "task": O.get("title"),
+            "complted": O.get("completed"),
+            "username": user.get("username")
+        } for O in requests.get(url + "todos",
+                                params={'userId': user.get("id")}).json()]
+        for user in users}, f)
