34805695c812fdab9c954d7a848551dd4b7a1316
RicardoBarretoR <richardbarrodriguez@hotmail.com>
2020-09-14 23:40:33 -0500

    adding task 1:export data in the CSV format

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,1 +1,19 @@
 #!/usr/bin/python3
+"""export data in the CSV format."""
+import csv
+import json
+import requests
+import sys
+
+if __name__ == '__main__':
+    user_id = sys.argv[1]
+    url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(url + 'users/{}'.format(user_id)).json()
+    todos = requests.get(url + 'todos', params={"userId": user_id}).json()
+    username = user.get("username")
+
+    with open("{}.CSV".format(user_id), 'w', newline="") as f:
+        writer = csv.writer(f, quoting=csv.QUOTE_ALL)
+        [writer.writerow(
+            [user_id, username, O.get("completed"), O.get("title")]
+        ) for O in todos]
