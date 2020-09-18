66f4fb5b461ad6e0e921453f4b63bb57016adb0f
RicardoBarretoR <richardbarrodriguez@hotmail.com>
2020-09-17 21:39:41 -0500

    adding task Advanced

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -7,21 +7,23 @@ def count_words(subreddit, word_list):
     """
     parses the title of all hot articles, and prints a sorted count
     """
-    hotp = requests.get('https://www.reddit.com/r/{}/hot.json'
-                        .format(subreddit),
-                        headers={"User-Agent": 'Mozilla/5.0 (X11; Linux x86_64) ' \
-                                                'AppleWebKit/537.36 (KHTML, like Gecko) ' \
-                                                'Chrome/51.0.2704.103 Safari/537.36'},
-                        allow_redirects=False,
-                        params={'limit': 100})
-    if hotp.status_code != 200:
+    h = requests.get('https://www.reddit.com/r/{}/hot.json'.format(subreddit),
+                     headers={"User-Agent": 'Mozilla/5.0 '
+                                            '(X11; Linux x86_64)'
+                                            'AppleWebKit/537.36'
+                                            '(KHTML, like Gecko)'
+                                            'Chrome/51.0.2704.103'
+                                            'Safari/537.36'},
+                     allow_redirects=False,
+                     params={'limit': 100})
+    if h.status_code != 200:
         return
     else:
         my_dict = {}
-        hotp = hotp.json()
+        h = h.json()
         for word in word_list:
             counter = 0
-            for post in hotp['data']['children']:
+            for post in h['data']['children']:
                 counter += post['data']['title'].count(word)
             if counter != 0:
                 my_dict[word] = counter
