c3586ca4e311528e0d4f8f8c2abc5140a3a9bb4c
RicardoBarretoR <richardbarrodriguez@hotmail.com>
2020-09-18 09:43:00 +0000

    adding task 2: returns the number of subscribers for a given subreddit

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,1 +1,29 @@
 #!/usr/bin/python3
+"""Function that queries the Reddit API"""
+import requests
+
+
+def recurse(subreddit, hot_list=[], after=None):
+    """
+    returns a list containing the titles of all hot articles
+    """
+    url = 'https://www.reddit.com/r/' + subreddit + '/hot.json'
+    user_agent_header = 'Mozilla/5.0 (X11; Linux x86_64) ' \
+                        'AppleWebKit/537.36 (KHTML, like Gecko) ' \
+                        'Chrome/51.0.2704.103 Safari/537.36'
+    limit_posts = 100
+    r = requests.get(url, allow_redirects=False,
+                     headers={'User-Agent': user_agent_header},
+                     params={'limit': limit_posts, 'after': after})
+    if r.status_code == 200:
+        try:
+            data = r.json()
+            list_hot_posts = data.get('data', None).get('children', None)
+            after = data.get('data', None).get('after', None)
+            for post in list_hot_posts:
+                hot_list.append(post.get('data').get('title'))
+            if after:
+                hot_list = recurse(subreddit, hot_list, after)
+        except:
+            pass
+        return hot_list
