95b8d493f43ce933fc2b54f29af31eb09cec330a
RicardoBarretoR <richardbarrodriguez@hotmail.com>
2020-09-18 00:48:01 -0500

    fixed task 2

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,20 +1,20 @@
 #!/usr/bin/python3
 """ export data in the JSON format"""
-import csv
 import json
 import requests
-import sys
+from sys import argv
 
 if __name__ == '__main__':
-    user_id = sys.argv[1]
-    url = "https://jsonplaceholder.typicode.com/"
-    user = requests.get(url + 'users/{}'.format(user_id)).json()
-    todos = requests.get(url + 'todos', params={"userId": user_id}).json()
-    username = user.get("username")
-
-    with open("{}.json".format(user_id), 'w', newline="") as f:
-        json.dump({user_id: [{
-            "task": O.get("title"),
-            "complted": O.get("completed"),
-            "username": username
-        } for O in todos]}, f)
+    url = 'https://jsonplaceholder.typicode.com/users/' + argv[1]
+    username = requests.get(url).json().get('username')
+    url = 'https://jsonplaceholder.typicode.com/todos?userId=' + argv[1]
+    tasks = requests.get(url).json()
+    ls = []
+    for task in tasks:
+        dic = {}
+        dic['task'] = task['title']
+        dic['completed'] = task['completed']
+        dic['username'] = username
+        ls.append(dic)
+    with open(argv[1]+'.json', 'w') as f:
+        json.dump({argv[1]: ls}, f)
